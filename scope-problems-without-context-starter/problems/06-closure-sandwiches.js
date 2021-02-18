545fd3071b92ded07ccecce6581901e0f756b9e8
root <root@DESKTOP-BLCL28L.localdomain>
2021-02-18 14:40:08 -0800

    sandwichmaker

--- scope-problems-without-context-starter/problems/06-closure-sandwiches.js
+++ scope-problems-without-context-starter/problems/06-closure-sandwiches.js
@@ -1,6 +1,6 @@
 /***********************************************************************
 Let's practice writing closures by creating a function named `sandwichMaker()`.
-This function will return another function that will accept a string to add 
+This function will return another function that will accept a string to add
 to the sandwich order (which will start off with a default "tomato" ingredient),
 separating each incoming ingredient with "and".
 
@@ -11,14 +11,39 @@ sandwich("spinach") // => "One sandwich with tomato and spinach"
 sandwich("jelly") // => "One sandwich with tomato and spinach and jelly"
 sandwich("bread") // => "One sandwich with tomato and spinach and jelly and bread"
 
-Another Sandwich:
+
 let sandwich2 = sandwichMaker(); // => returns a function
 sandwich2("pb") // => "One sandwich with tomato and pb"
 
 ***********************************************************************/
+// write a function that will accept a string to add to the sandwich order.
+// set up an array that only contains tomato
+const sandwichMaker = () => {
+  let ingredients = ['tomato']
+  return newIngredient => {
+    ingredients.push(newIngredient)
+    let result = "One sandwich with "
+    ingredients.forEach((ele, i) => {
+      result += ele + " "
+      if (i !== ingredients.length - 1) {
+        result += "and "
+      }
+    })
+    return result
+  }
 
-// your code here!
 
+
+}
+
+let sandwich = sandwichMaker(); // => returns a function
+sandwich("spinach")// => "One sandwich with tomato and spinach"
+sandwich("jelly") // => "One sandwich with tomato and spinach and jelly"
+sandwich("bread") // => "One sandwich with tomato and spinach and jelly and bread"
+
+
+let sandwich2 = sandwichMaker(); // => returns a function
+sandwich2("pb") // => "One sandwich with tomato and pb"
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
